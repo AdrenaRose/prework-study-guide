57fe7b0e894b2ed077366b3c15f0a8153959de61
AdrenaRose <Charlenejparker81@gmail.com>
2023-02-11 18:55:06 -0500

    added JS

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
-var topics = "Programming, HTML, CSS, Git, JavaScript";
+/*var topics = "Programming, HTML, CSS, Git, JavaScript";
 /*created variable called topics, which contains a string that includes the topics covered so far
 /*strings are always contained in quotation marks
 /*each line of JS ends with a semicolon
 */
-console.log(topics);
+/*console.log(topics);
 /*console.log() - console refers to a test environment developers use to check code
 .log() is a method, or a set of instructions that can be executed by a computer
 the .log() method outputs whatever we add inside the parentheses to the console*/
+var topic = "Programming";
+
+if (topic === "Programming") {
+  console.log("Let's study Programming!");
+} else if (topic === "HTML") {
+  console.log("Let's study HTML!");
+} else if (topic === "CSS") {
+  console.log("Let's study CSS!");
+} else if (topic === "Git") {
+  console.log("Let's study Git!");
+} else if (topic === "JavaScript") {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log("Please try again!");
+}
